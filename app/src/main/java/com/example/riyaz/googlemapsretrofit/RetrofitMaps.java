cbfcf9709ebf7ba9016ffd60254147f3e1c532ab
hiraniriyaz <hiraniriyaz@gmail.com>
2017-07-20 00:53:39 -0400

    my commit

---
+++ app/src/main/java/com/example/riyaz/googlemapsretrofit/RetrofitMaps.java
@@ -0,0 +1,20 @@
+package com.example.riyaz.googlemapsretrofit;
+
+/**
+ * Created by Riyaz on 7/19/2017.
+ */
+import com.example.riyaz.googlemapsretrofit.POJO.Example;
+
+import retrofit.Call;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+public interface RetrofitMaps {
+
+    /*
+     * Retrofit get annotation with our URL
+     */
+    @GET("api/place/nearbysearch/json?sensor=true&key=AIzaSyDN7RJFmImYAca96elyZlE5s_fhX-MMuhk")
+    Call<Example> getNearbyPlaces(@Query("type") String type, @Query("location") String location, @Query("radius") int radius);
+
+}
